93693e7fed80213b84efdef251e4860620c02a8f
Julia Hoge <17hogeju@gmail.com>
2021-08-01 19:33:54 -0500

    Fix sql

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -6,7 +6,7 @@ exports.findCategorySales = (req, res) => {
     sequelize.query(`select sum(ta.spend), trim(p.commodity) as "commodity",
     concat(extract(month from ta.purchase_date), '-20', extract(year from ta.purchase_date)) as "month/year" 
     from kroger.transactions as ta
-    inner join kroger.products p on cast(p.product_num as int) = ta.product_num 
+    inner join kroger.products p on p.product_num = ta.product_num 
     group by p.commodity, extract(year from ta.purchase_date), extract(month from ta.purchase_date)`).then(data => {
       res.send(data);
     })
